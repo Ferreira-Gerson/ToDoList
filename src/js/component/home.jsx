1b809946d6526aae75357713ea7ce86e4a3d6585
Ferreira-Gerson <gersonferreira90@gmail.com>
2022-09-14 22:02:41 +0000

    First commit without aesthetic edits

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,36 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [ task , setTask ] = useState();
+	const [ listtask, setListTask ] = useState ([]);
+	
+	const catchValue = (evnt) => {
+		if (evnt.keyCode === 13) {
+			setListTask([...listtask, evnt.target.value]);
+			evnt.target.value = "";
+		
+		}
+	};
+	console.log(listtask);
+	
+	const deleteItem = (index) => () =>{
+		setListTask((item) => item.filter((_, item) => item !== index));	
+	}
+	console.log(deleteItem)
+	const list = listtask.map((item, index) => {
+		return (
+			<li key={index} onClick={deleteItem(index)}>{item}</li>
+		)
+	});
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center">
+			<h1 className="text-center mt-5">TODOS</h1>
+  				<input className="text" onKeyDown={catchValue} placeholder="Please add your task" />
+			<ul className="list-group aline-center">
+				<li>{list}</li>
+			</ul>
 		</div>
 	);
 };
